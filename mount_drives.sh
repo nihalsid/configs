0975a0a6c1d27f481da9d82f6068fcece1817c2d
Yawar Nihal Siddiqui <yawarnihal@gmail.com>
2023-05-08 21:01:20 +0200

    Add files via upload

---
+++ mount_drives.sh
@@ -0,0 +1,4 @@
+sudo sshfs -o allow_other ysiddiqui@rohan.vc.in.tum.de:/cluster/gondor/ysiddiqui /cluster/gondor/ysiddiqui
+sudo sshfs -o allow_other ysiddiqui@rohan.vc.in.tum.de:/rhome/ysiddiqui/ /rhome/ysiddiqui
+sudo sshfs -o allow_other ysiddiqui@rohan.vc.in.tum.de:/cluster_HDD/gondor/ysiddiqui/ /cluster_HDD/gondor/ysiddiqui
+sudo sshfs -o allow_other ysiddiqui@rohan.vc.in.tum.de:/cluster/gimli/ysiddiqui/ /cluster/gimli/ysiddiqui
